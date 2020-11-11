627c5bbcfae8ad916f26dfdfdb7d85b4e8b7f165
Aqudi <gjdigj145@gmail.com>
2020-11-12 02:30:49 +0900

    fix: Update deepDarkGreen color

--- lib/utils/palette.dart
+++ lib/utils/palette.dart
@@ -2,7 +2,7 @@ import 'package:alcolpedia_mobile/utils/hex_color.dart';
 import 'package:flutter/material.dart';
 
 class Palette {
-  static final Color deepDarkGreen = HexColor('#67764D').withOpacity(0.53);
+  static final Color deepDarkGreen = HexColor('#323433').withOpacity(0.53);
   static final Color darkGreen = HexColor('#67764D').withOpacity(0.62);
   static final Color green = HexColor('#96A375').withOpacity(0.62);
   static final Color ivory = HexColor('#F4EED8').withOpacity(0.62);
