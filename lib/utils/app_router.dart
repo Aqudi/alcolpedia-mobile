9a9449daf0bd603e687e8680231b52059ec0b1b7
Aqudi <gjdigj145@gmail.com>
2020-11-12 01:52:38 +0900

    fix: AppRouter pages

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -5,17 +5,17 @@ import 'package:get/get.dart';
 class AppRouter {
   static final route = [
     // home
-    GetPage(name: Routes.homePage, page: () => LoginPage()),
+    GetPage(name: Routes.homePage, page: () => HomePage()),
 
     // auth
     GetPage(name: Routes.loginPage, page: () => LoginPage()),
     GetPage(name: Routes.signupPage, page: () => SignupPage()),
 
     // content
-    GetPage(name: Routes.gamePage, page: () => LoginPage()),
-    GetPage(name: Routes.musicPage, page: () => LoginPage()),
-    GetPage(name: Routes.recipePage, page: () => LoginPage()),
-    GetPage(name: Routes.battlecryPage, page: () => LoginPage()),
-    GetPage(name: Routes.optionPage, page: () => LoginPage()),
+    GetPage(name: Routes.gamePage, page: () => GamePage()),
+    GetPage(name: Routes.musicPage, page: () => MusicPage()),
+    GetPage(name: Routes.recipePage, page: () => RecipePage()),
+    GetPage(name: Routes.battlecryPage, page: () => BattleCryPage()),
+    GetPage(name: Routes.optionPage, page: () => OptionPage()),
   ];
 }
