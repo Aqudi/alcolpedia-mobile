13ceded975af655de44be3c104d5de444ecd7b64
Aqudi <gjdigj145@gmail.com>
2020-11-30 22:49:47 +0900

    feat: Apply local scaffold

--- lib/pages/content/recipe/recipe_page.dart
+++ lib/pages/content/recipe/recipe_page.dart
@@ -1,8 +1,17 @@
+import 'package:alcolpedia_mobile/pages/content/local_widgets/content_scaffold.dart';
+import 'package:alcolpedia_mobile/pages/content/local_widgets/content_title.dart';
+import 'package:alcolpedia_mobile/utils/palette.dart';
 import 'package:flutter/material.dart';
 
 class RecipePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ContentScaffold(
+      appBarBackgroundColor: Palette.darkGreen,
+      contentTitle: ContentTitle(
+        iconData: Icons.science,
+        title: "폭탄주 레시피",
+      ),
+    );
   }
 }
