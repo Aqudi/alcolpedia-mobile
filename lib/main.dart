0b6ca1763af9204134e22d07e3566ac694a34d3c
Aqudi <gjdigj145@gmail.com>
2020-12-29 02:36:49 +0900

    feat: Apply riverpod provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
+import 'package:hooks_riverpod/all.dart';
 
 import 'utils/app_router.dart';
 import 'utils/app_routes.dart';
 import 'utils/palette.dart';
 
 void main() {
-  runApp(AlcolpediaApp());
+  runApp(
+    ProviderScope(
+      child: AlcolpediaApp(),
+    ),
+  );
 }
 
-class AlcolpediaApp extends StatelessWidget {
+class AlcolpediaApp extends HookWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
