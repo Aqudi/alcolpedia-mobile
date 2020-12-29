b3b8efda6e2e9c6b07616b74ce605bd92ea69cfc
Aqudi <gjdigj145@gmail.com>
2020-12-30 02:01:26 +0900

    feat: Apply tag service and provider

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,10 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
 
+import '../../models/tag.dart';
+import '../../providers/tag_provider.dart';
 import '../../utils/background_gradient.dart';
 import '../../utils/palette.dart';
 import '../../widgets/alcol_hash_chip.dart';
@@ -10,9 +13,35 @@ import 'local_widgets/home_searchbar.dart';
 import 'local_widgets/home_title.dart';
 import 'local_widgets/widgets.dart';
 
-class HomePage extends StatelessWidget {
-  final Faker faker = Faker();
+class TagList extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      context.read(tagViewModel).initState();
+      return context.read(tagViewModel).dispose;
+    }, []);
 
+    final tags = useProvider(tagProvider).state;
+
+    if (tags == null) {
+      return Container(child: const Center(child: Text("태그를 불러올 수 없습니다.")));
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(color: Colors.transparent),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 10.0,
+        runSpacing: 5.0,
+        children: List.generate(
+            tags.length, (index) => AlcolHashChip(tag: tags[index].title)),
+      ),
+    );
+  }
+}
+
+class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -55,14 +84,7 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(color: Colors.transparent),
-                    child: Wrap(
-                      alignment: WrapAlignment.center,
-                      children: List.generate(40,
-                          (index) => AlcolHashChip(tag: faker.person.name())),
-                    ),
-                  ),
+                  TagList(),
                 ],
               ),
             ),
