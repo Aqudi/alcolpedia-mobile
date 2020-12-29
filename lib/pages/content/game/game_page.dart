d84cc88c13ee2ef047d32dfc36c04a89463cf49a
Aqudi <gjdigj145@gmail.com>
2020-12-30 02:53:47 +0900

    feat: Create GameList and GameDetail

--- lib/pages/content/game/game_page.dart
+++ lib/pages/content/game/game_page.dart
@@ -1,9 +1,8 @@
-import 'dart:math';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../models/content.dart';
@@ -20,46 +19,135 @@ class GameTile extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final content = useProvider(_currentContent);
-    final bodyList = content.body.split("\n");
-    final end = min(bodyList.length, 3);
-    final body = bodyList.sublist(0, end).join("\n");
     return Container(
-      height: 150,
-      padding: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 15,
-      ),
       child: Card(
-        child: Row(
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              child: CachedNetworkImage(
-                imageUrl: "$baseUrl${content.image}",
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-              ),
+        elevation: 2,
+        child: InkWell(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 5,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                Text(
-                  content.title.trimLeft(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      child: CachedNetworkImage(
+                        imageUrl: "$baseUrl${content.image}",
+                        placeholder: (context, url) =>
+                            CircularProgressIndicator(),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          content.title.trim(),
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: _buildLikeButtonAndText(content.like.length),
+                    ),
+                  ],
                 ),
-                Flexible(
+                Align(
+                  alignment: Alignment.topRight,
+                  child: _buildBookmarkButton(),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            Get.dialog(_buildDetailDialog(content));
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(icon) {
+    return InkWell(
+      customBorder: CircleBorder(),
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Icon(
+          icon,
+          size: 25,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBookmarkButton() {
+    return _buildIcon(Icons.bookmark_border_outlined);
+  }
+
+  Widget _buildLikeButtonAndText(like) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildIcon(Icons.favorite_border_outlined),
+        Text(
+          " $like",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailDialog(Content content) {
+    final body = content.body.replaceAll("/media/", "$baseUrl/media/");
+    return AlertDialog(
+      content: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            child: InkWell(
+              customBorder: CircleBorder(),
+              child: Icon(
+                Icons.close,
+                size: 30,
+              ),
+              onTap: () {
+                Get.back();
+              },
+            ),
+          ),
+          Column(
+            children: [
+              Text(content.title, style: Get.theme.textTheme.headline5),
+              Divider(),
+              Flexible(
+                child: SingleChildScrollView(
                   child: MarkdownBody(
                     data: body,
                   ),
                 ),
-              ],
-            ),
-            Icon(Icons.favorite),
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
@@ -87,7 +175,14 @@ class GamePage extends HookWidget {
         iconData: Icons.wine_bar,
         title: "엄준식이 좋아하는 랜덤게임",
       ),
-      body: ListView.builder(
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 300.0,
+          // crossAxisSpacing: 10,
+          // mainAxisSpacing: 10,
+        ),
         itemCount: contents.length,
         itemBuilder: (ctx, idx) => ProviderScope(
           overrides: [
