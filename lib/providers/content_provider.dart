355348f2ef7e32a58d350289520b6e9decdfa570
Aqudi <gjdigj145@gmail.com>
2020-12-30 18:09:45 +0900

    feat: Update content_provider

--- lib/providers/content_provider.dart
+++ lib/providers/content_provider.dart
@@ -1,17 +1,22 @@
+import 'package:hooks_riverpod/all.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../models/content.dart';
 import '../services/content_service.dart';
 import '../utils/content_type.dart';
 
-final contentsProvider = StateProvider<List<Content>>((ref) => null);
+StateProvider<List<Content>> getContentsProvider() =>
+    StateProvider<List<Content>>((ref) => null);
 
-final contentsViewModel =
-    Provider.autoDispose((ref) => ContentViewModel(ref.read));
+AutoDisposeProvider<ContentViewModel> getContentsViewModel(
+        StateProvider<List<Content>> contentsProvider) =>
+    Provider.autoDispose((ref) => ContentViewModel(ref.read, contentsProvider));
 
 class ContentViewModel {
   final Reader read;
-  ContentViewModel(this.read);
+  final StateProvider<List<Content>> contentsProvider;
+
+  ContentViewModel(this.read, this.contentsProvider);
 
   void initState(ContentType contentType) async {
     read(contentsProvider).state =
